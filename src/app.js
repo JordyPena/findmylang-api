78154d0692404fe70a664a0c8d2195f638462829
Jordy Pena <jordypenamontes@gmail.com>
2020-11-08 15:15:51 -0600

    cors added

--- src/app.js
+++ src/app.js
@@ -2,34 +2,38 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const errorHandler = require("./error-handler");
 
 const app = express();
 
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganSetting));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 app.use((error, req, res, next) => {
-  let response
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    response = { error }
+    response = { error };
   }
-  res.status(500).json(response)
-})
-
-const PORT = process.env.PORT || 3000
-
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!")
+  res.status(500).json(response);
 });
 
-app.use(errorHandler)
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use(errorHandler);
 
 module.exports = app;
