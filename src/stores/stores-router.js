560a0f8edda77bbf8ef2bbe2b5554a3a7ee21a71
Jordy Pena <jordypenamontes@gmail.com>
2020-11-10 14:35:05 -0600

    route added

--- src/stores/stores-router.js
+++ src/stores/stores-router.js
@@ -1,23 +1,46 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const xss = require('xss');
+const xss = require("xss");
 const StoresService = require("./stores-service");
 const logger = require("../logger");
-
 
 const storesRouter = express.Router();
 const jsonParser = express.json();
 
 storesRouter
   .route("/")
+
   .get((req, res, next) => {
-    const knexInstance = req.app.get("db");
-    StoresService.getByName(knexInstance)
-      .then((employees) => {
-        res.json(employees);
+    StoresService.getAll(req.app.get("db"))
+      .then((stores) => {
+        res.json(stores);
+      })
+      .catch(next);
+  });
+
+storesRouter
+  .route("/:language")
+  .all((req, res, next) => {
+    console.log("request is", req.params);
+    const { language } = req.params;
+    console.log("this is my lang", language);
+    StoresService.getByLanguage(req.app.get("db"), language)
+      .then((store) => {
+        if (!store) {
+          logger.error(`Store with language ${language} not found.`);
+          return res.status(404).json({
+            error: { message: `Store Not Found` },
+          });
+        }
+
+        res.store = store;
+        next();
       })
       .catch(next);
   })
 
-module.exports = storesRouter;
+  .get((req, res) => {
+    res.json(res.store);
+  });
 
+module.exports = storesRouter;
--- src/stores/stores-service.js
+++ src/stores/stores-service.js
@@ -1,41 +1,25 @@
- const StoresService = {
-   getByEmployee(db) {
-     return db
-      .join('employees', 'stores.id',)
-
-   },
-   
-   getAllStore(db) {
+const StoresService = {
+  getByLanguage(db, languages) {
     return db
-     .select('*')
-     .from('stores')
+      .select('*')
+      .from("stores")
+      .where("languages", "like", `%${languages}%`)
+      
   },
+  //with this where i cant compare exactly
+  //because my seed is a string of multiple langs
+  //so within this string of multiple langs in my seed does
+  //this lang match ""like"""
+  //the reason for the template literal i need
+  //to sandwhich my value in %
+  //and in order to get my value
+  //i need to use my template literal
 
-  getByName(db, name) {
+  getAll(db) {
     return db
       .select('*')
       .from('stores')
-      .where({name: name})
   }
-
-  getByLang(db, lang) {
-    return db
-      .select(*)
-      .from(stores)
-      //with this where i cant compare exactly
-      //because my seed is a string of multiple langs 
-      //so within this string of multiple langs in my seed does
-      //this lang match ""like"""
-      //the reason for the template literal i need
-      //to sandwhich my value in % 
-      //and in order to get my value 
-      //i need to use my template literal
-      .where('languages', 'like', `%${lang}%`)
-      
-  }
-   
 };
 
- 
-
- module.exports = StoresService;
+module.exports = StoresService;
