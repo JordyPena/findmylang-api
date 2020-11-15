dc92478dfc3e4d1ddff89afba732c0cfdbebaf88
Jordy Pena <jordypenamontes@gmail.com>
2020-11-15 14:40:29 -0600

    database update

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/mylang',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/mylang',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/stores',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/stores',
  
 }
 
