9ed56d8ab04fc79bab2c84edd8b29b9a17227d31
Jordy Pena <jordypenamontes@gmail.com>
2021-04-12 19:48:50 -0500

    removed all logger imports and uses

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # FindMyLang
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Live link 
 
 https://find-my-lang-app.vercel.app/
--- src/stores/stores-validator.js
+++ src/stores/stores-validator.js
@@ -1,8 +1,8 @@
-const logger = require("../logger");
+
 
 function getStoreValidationError({ language }) {
   if (!language) {
-    logger.error(`${language} must be selected`);
+  
     return {
       error: {
         message: `language must be selected`,
